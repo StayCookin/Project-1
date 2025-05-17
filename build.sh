821d6698c7381243e0177b7a60bd715db3016bc0
Lethabo <Lethaboswereki029@gmail.com>
2025-05-18 00:35:03 +0200

    rolled back changes after they messed up the formatting

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Building static site..."
-# Copy files to output directory
-mkdir -p .vercel/output/static
-cp index.html .vercel/output/static/
-cp main.js .vercel/output/static/
-cp -r assets .vercel/output/static/
