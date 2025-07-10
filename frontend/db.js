b787004c8d204b92de4518fdf03eb4e1bc73d273
Lethabo <Lethaboswereki029@gmail.com>
2025-07-10 20:36:35 +0200

    db

--- frontend/db.js
+++ frontend/db.js
@@ -1,15 +1,15 @@
 // db.js
 // MySQL connection setup for Node.js backend
-require ("dotenv").config();
+
 const mysql = require("mysql2");
 
 // Update these credentials as needed
 dbConfig = {
   host: process.env.DB_HOST || "34.135.188.4",
-  user: process.env.DB_USER || "Lethabo",
-  password: process.env.DB_PASSWORD || "76068664Hanzu!",
-  database: process.env.DB_NAME || "inrent_db",
-  port: process.env.DB_PORT|| 3306
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "77379041Hanzu!",
+  database: process.env.DB_NAME || "inrent-mysql-instance",
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT): 3306
 };
 
 const pool = mysql.createPool(dbConfig);
