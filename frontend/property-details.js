8346d9ac3eab9b8edb47da5b8541ec0dbd8f3820
Lethabo <Lethaboswereki029@gmail.com>
2025-09-23 08:17:02 +0200

    revert changes

--- frontend/property-details.js
+++ frontend/property-details.js
@@ -1,13 +1,33 @@
-async function initializeFirebase(){
-    try {
-        const response = await fetch ('/api/firebase-config');
-        const firebaseConfig = await response.json();
+import firebase from "firebase/compat/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import {
+    getAuth,
+    onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    collection,
+    query,
+    where,
+    getDocs,
+    addDoc,
+    deleteDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
 
-        firebase.initializeApp(firebaseConfig);
-        } catch (error) {
-            console.error('Error loading firebase config:',error);
-        }
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyCZuEC4QU-RYxQbjWqBoxk6j1mbwwRtRBo",
+    authDomain: "inrent-6ab14.firebaseapp.com",
+    databaseURL: "https://inrent-6ab14-default-rtdb.firebaseio.com",
+    projectId: "inrent-6ab14",
+    storageBucket: "inrent-6ab14.firebasestorage.app",
+    messagingSenderId: "327416190792",
+    appId: "1:327416190792:web:970377ec8dcef557e5457d",
+    measurementId: "G-JY9E760ZQ0"
+};
+
 let app, db, auth;
 let currentUser = null;
 let currentProperty = null;
